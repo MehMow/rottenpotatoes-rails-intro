29de503ef259edf0580af8ac616d0ea7daa1799d
Guillermo Gonzalez <guillermo.gonzalez@berkeley.edu>
2020-10-23 23:24:08 +0000

    trying to fix redirect...

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,12 +10,18 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings 
     @movies = Movie.all
     
+    
+    
     if params[:ratings]==nil && params[:sort_title]==nil && params[:sort_date]==nil
       if session[:ratings] == nil
         session[:ratings] = []       
-        redirect_to movies_path(:sort_date => session[:sort_date], :sort_title => session[:sort_title], :ratings => session[:ratings]  )
       end
+      redirect_to movies_path(:sort_date => session[:sort_date], :sort_title => session[:sort_title], :ratings => session[:ratings]  )
+      
     end
+    
+    
+    
     
     if params[:home] == nil #that means coming from somewhere else; so reload session
       #if session[:ratings] == nil 
